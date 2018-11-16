d4138b3cf2be7439861ef2df2f05080420e3a63e
Chidiebere <chidex5@gmail.com>
2018-11-16 16:42:07 +0100

    Updates

---
+++ precache-manifest.bf11df549d7ef5d9ff892e803d17360e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c7c7c7b7fc7b90b60e04",
+    "url": "/Airbnb-Clone/static/js/runtime~main.c7c7c7b7.js"
+  },
+  {
+    "revision": "acb72f73b03aecb42eb3",
+    "url": "/Airbnb-Clone/static/js/main.acb72f73.chunk.js"
+  },
+  {
+    "revision": "23a2f328cb6858b95c34",
+    "url": "/Airbnb-Clone/static/js/1.23a2f328.chunk.js"
+  },
+  {
+    "revision": "acb72f73b03aecb42eb3",
+    "url": "/Airbnb-Clone/static/css/main.83eb74fa.chunk.css"
+  },
+  {
+    "revision": "9e190f0e683d6878107472a8f3d4d230",
+    "url": "/Airbnb-Clone/index.html"
+  }
+];
--- src/components/hello.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-class Hello extends React.Component {
-    render() {
-        return <div>Hello {this.props.firstName} {this.props.lastName} </div>
-    }
-}
-
-export default Hello;
